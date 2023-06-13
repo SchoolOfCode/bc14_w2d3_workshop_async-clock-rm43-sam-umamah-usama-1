537e3fbeab266f49daede04a509117f197b192f1
usama-asghar1 <usama.asghar@hotmail.co.uk>
2023-06-13 11:53:30 +0100

    changes

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 const hand = document.querySelector(".hand");
 
-hand.style.transform = `rotate(90deg)`; //🔍 HINT: THis is an example of how to rotate the hand!
+//hand.style.transform = `rotate(90deg)`; //🔍 HINT: THis is an example of how to rotate the hand!
+
+
+function tick() {
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const minutes = now.getMinutes();
+  const hours = now.getHours();
+  const secondsDegree = ((seconds / 60) * 360) + 90;
+  const minutesDegree = ((minutes / 60) * 360) + ((seconds / 60) * 6) + 90;
+  const hoursDegree = ((hours / 12) * 360) + ((minutes / 60) * 30) + 90;
+  hand.style.transform = `rotate(${secondsDegree}deg)`;
+}
+
+setInterval(tick, 1000);
